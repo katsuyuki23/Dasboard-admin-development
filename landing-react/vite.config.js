2443e602e71a3f1cb63ea66488c1a9e0517b397a
Admin <admin@example.com>
2026-02-15 11:14:06 +0700

    Fix: Update vite config to use dist folder in Vercel environment

--- landing-react/vite.config.js
+++ landing-react/vite.config.js
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-    plugins: [react()],
-    base: '/landing-assets/',
-    build: {
-        outDir: '../public/landing-assets',
-        emptyOutDir: true,
-    },
-    server: {
-        port: 5173,
-    }
-}))
+export default defineConfig(({ command, mode }) => {
+    const isVercel = process.env.VERCEL === '1';
+
+    return {
+        plugins: [react()],
+        base: isVercel ? '/' : '/landing-assets/',
+        build: {
+            outDir: isVercel ? 'dist' : '../public/landing-assets',
+            emptyOutDir: true,
+        },
+        server: {
+            port: 5173,
+        }
+    };
+})
